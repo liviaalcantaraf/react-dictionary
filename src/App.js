a6ab5b0fe482a410f89593f76a47a7296c77882a
liviaalcantaraf <liviaalcantaraf@gmail.com>
2021-12-11 18:48:23 +0000

    Added Meaning, Phonetics and results of the dictionary

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.png";
+import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-img" alt="appimg" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+
+        <footer className="App-footer">Coded by Lívia A.</footer>
+      </div>
     </div>
   );
 }
---
+++ src/Dictionary.js
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import Results from "./Results";
+import axios from "axios";
+import "./Dictionary.css";
+
+export default function Dictionary() {
+  let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState(null);
+
+  function handleResponse(response) {
+    setResults(response.data[0]);
+    console.log(response.data[0]);
+    console.log(response.data[0].meanings[0].definitions[0].definition);
+  }
+
+  function search(event) {
+    event.preventDefault();
+
+    // documentation: https://dictionaryapi.dev/
+
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+
+    axios.get(apiUrl).then(handleResponse);
+  }
+
+  function handleKeywordChange(event) {
+    setKeyword(event.target.value);
+  }
+
+  return (
+    <div className="Dictionary">
+      <form onSubmit={search}>
+        <input type="search" onChange={handleKeywordChange} />
+      </form>
+
+      <Results results={results} />
+    </div>
+  );
+}
