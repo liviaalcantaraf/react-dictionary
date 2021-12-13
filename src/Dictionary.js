e2992ebaf35713a82e68fc96597d82fcf115ca47
liviaalcantaraf <liviaalcantaraf@gmail.com>
2021-12-13 16:00:07 +0000

    added photos to the dictionary result

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Results from "./Results";
+import Photos from "./Photos";
 import axios from "axios";
 import "./Dictionary.css";
 
@@ -7,11 +8,16 @@ export default function Dictionary(props) {
   let [keyword, setKeyword] = useState(props.defaultKeyword);
   let [results, setResults] = useState(null);
   let [loaded, setLoaded] = useState(false);
+  let [photos, setPhotos] = useState(null);
 
   function handleResponse(response) {
     setResults(response.data[0]);
     console.log(response.data[0]);
     console.log(response.data[0].meanings[0].definitions[0].definition);
+  }
+  function handlePexelsResponse(response) {
+    console.log(response.data);
+    setPhotos(response.data.photos);
   }
 
   function search() {
@@ -20,6 +26,12 @@ export default function Dictionary(props) {
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
 
     axios.get(apiUrl).then(handleResponse);
+
+    let pexelsApiKey =
+      "563492ad6f9170000100000186e6c472eacf4feaa598eeee066daafb";
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
+    let headers = { Authorization: `Bearer ${pexelsApiKey}` };
+    axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
   }
 
   function handleSubmit(event) {
@@ -54,6 +66,7 @@ export default function Dictionary(props) {
         </section>
 
         <Results results={results} />
+        <Photos photos={photos} />
       </div>
     );
   } else {
--- src/Phonetic.js
+++ src/Phonetic.js
@@ -2,10 +2,9 @@ import React from "react";
 import "./Phonetic.css";
 
 export default function Phonetic(props) {
-  console.log(props.phonetic);
   return (
     <div className="Phonetic">
-      <a href={props.phonetic.audio} target="_blank">
+      <a href={props.phonetic.audio} target="noreferrer">
         Listen
       </a>
 
