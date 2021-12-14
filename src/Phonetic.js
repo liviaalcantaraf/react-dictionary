c092e1b85294cb5ed17265313f6960972692fb02
liviaalcantaraf <liviaalcantaraf@gmail.com>
2021-12-14 12:48:46 +0000

    changed the phonetic audio layout

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -1,14 +1,20 @@
 import React from "react";
 import "./Phonetic.css";
+import ReactAudioPlayer from "react-audio-player";
 
 export default function Phonetic(props) {
   return (
     <div className="Phonetic">
-      <a href={props.phonetic.audio} target="noreferrer">
-        Listen
-      </a>
-
-      <span className="text">{props.phonetic.text}</span>
+      <div className="col-2">
+        <span className="text">{props.phonetic.text}</span>
+        <p className="mt-3">
+          <ReactAudioPlayer
+            src={props.phonetic.audio}
+            controls
+            className="audio"
+          />
+        </p>
+      </div>
     </div>
   );
 }
