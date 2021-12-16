35bad841a9fe961cd9524f5a35e40c2186815741
liviaalcantaraf <liviaalcantaraf@gmail.com>
2021-12-16 11:14:35 +0000

    corrected the phonetic audio

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -5,16 +5,9 @@ import ReactAudioPlayer from "react-audio-player";
 export default function Phonetic(props) {
   return (
     <div className="Phonetic">
-      <div className="col-2">
-        <span className="text">{props.phonetic.text}</span>
-        <p className="player mt-3">
-          <ReactAudioPlayer
-            src={props.phonetic.audio}
-            controls
-            className="audio"
-          />
-        </p>
-      </div>
+      <span className="text">{props.phonetic.text}</span>
+      <br />
+      <ReactAudioPlayer src={props.phonetic.audio} controls className="audio" />
     </div>
   );
 }
